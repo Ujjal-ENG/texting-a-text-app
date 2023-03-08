ec69c0a9d6dfe6e5d48b77f89f05a96255ad768c
user <ujjalroy7862@gmail.com>
2023-03-08 17:28:22 +0600

    added the another functionality

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const TextForm = (props) => {
+    const [text, setText] = useState('Enter text here');
+    const handleChange = (e) => {
+        setText(e.target.value);
+    };
+
+    let buttonClicked = true;
+    const handleClickUP = () => {
+        if (buttonClicked) {
+            setText(text.toUpperCase());
+            buttonClicked = false;
+        } else {
+            setText(text.toLowerCase());
+            // buttonClicked = true;
+        }
+    };
     return (
-        <div className="container mt-4">
-            <h1 className="text-center">{props.heading}</h1>
-            <div className="mb-3">
-                <label htmlFor="myBox" className="form-label">
-                    Write your text here
-                </label>
-                <textarea className="form-control" id="myBox" rows="8"></textarea>
+        <>
+            <div className="container mt-4">
+                <h1 className="text-center">{props.heading}</h1>
+                <div className="mb-3">
+                    <label htmlFor="myBox" className="form-label">
+                        Write your text here
+                    </label>
+                    <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleChange}></textarea>
+                </div>
+                <button className="btn btn-primary" onClick={handleClickUP}>
+                    {'Convert to UpperCase'}
+                </button>
             </div>
-            <button className="btn btn-primary">Convert to UpperCase</button>
-        </div>
+
+            {/* text summary */}
+            <div className="container mt-5">
+                <h3>Your Text Summary</h3>
+                <h4>
+                    {text.split(' ').length} words and {text.trim().length} characters without space!!
+                </h4>
+            </div>
+        </>
     );
 };
 
